55fd03f8d08e2c40cc8de2fc92331ed9d561a381
Nick Horton <nickkbhorton@gmail.com>
2024-11-13 13:06:05 +0000

    removing stuff an refactor

--- arrayalgebra.h
+++ arrayalgebra.h
@@ -6,8 +6,7 @@
 #include <iostream>
 #include <numeric>
 
-namespace aa
-{
+namespace aa {
 
 typedef float Float;
 typedef int Integer;
@@ -42,243 +41,246 @@ typedef std::array<std::array<Uinteger, 4>, 4> umat4;
 
 // Printing function
 template <typename T, std::size_t N>
-constexpr auto
-operator<<(std::ostream& os, std::array<T, N> const& a) -> std::ostream&
-{
-    for (size_t j = 0; j < N; j++) {
-        os << a[j] << " ";
-    }
-    return os;
+constexpr auto operator<<(std::ostream &os,
+                          std::array<T, N> const &a) -> std::ostream & {
+  for (size_t j = 0; j < N; j++) {
+    os << a[j] << " ";
+  }
+  return os;
 }
 
 // v1 + v2
 template <typename T, std::size_t N>
-constexpr auto operator+(std::array<T, N> const& a1, std::array<T, N> const& a2)
-    -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    for (size_t j = 0; j < N; j++) {
-        result[j] = a1[j] + a2[j];
-    }
-    return result;
+constexpr auto operator+(std::array<T, N> const &a1,
+                         std::array<T, N> const &a2) -> std::array<T, N> {
+  std::array<T, N> result{};
+  for (size_t j = 0; j < N; j++) {
+    result[j] = a1[j] + a2[j];
+  }
+  return result;
 }
 
 // m1 + m2
 template <typename T, std::size_t N, std::size_t M>
-constexpr auto operator+(
-    std::array<std::array<T, M>, N> const& m1,
-    std::array<std::array<T, M>, N> const& m2
-) -> std::array<std::array<T, M>, N>
-{
-    std::array<std::array<T, M>, N> result{};
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < M; j++) {
-            result[i][j] = m1[i][j] + m2[i][j];
-        }
+constexpr auto operator+(std::array<std::array<T, M>, N> const &m1,
+                         std::array<std::array<T, M>, N> const &m2)
+    -> std::array<std::array<T, M>, N> {
+  std::array<std::array<T, M>, N> result{};
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < M; j++) {
+      result[i][j] = m1[i][j] + m2[i][j];
     }
-    return result;
+  }
+  return result;
 }
 
 // v1 - v2
 template <typename T, std::size_t N>
-constexpr auto operator-(std::array<T, N> const& a1, std::array<T, N> const& a2)
-    -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    for (size_t j = 0; j < N; j++) {
-        result[j] = a1[j] - a2[j];
-    }
-    return result;
+constexpr auto operator-(std::array<T, N> const &a1,
+                         std::array<T, N> const &a2) -> std::array<T, N> {
+  std::array<T, N> result{};
+  for (size_t j = 0; j < N; j++) {
+    result[j] = a1[j] - a2[j];
+  }
+  return result;
 }
 
 // m1 - m2
 template <typename T, std::size_t N, std::size_t M>
-constexpr auto operator-(
-    std::array<std::array<T, M>, N> const& m1,
-    std::array<std::array<T, M>, N> const& m2
-) -> std::array<std::array<T, M>, N>
-{
-    std::array<std::array<T, M>, N> result{};
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < M; j++) {
-            result[i][j] = m1[i][j] - m2[i][j];
-        }
+constexpr auto operator-(std::array<std::array<T, M>, N> const &m1,
+                         std::array<std::array<T, M>, N> const &m2)
+    -> std::array<std::array<T, M>, N> {
+  std::array<std::array<T, M>, N> result{};
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < M; j++) {
+      result[i][j] = m1[i][j] - m2[i][j];
     }
-    return result;
+  }
+  return result;
 }
 
 // -v
 template <typename T, std::size_t N>
-constexpr auto operator-(std::array<T, N> const& a1) -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    for (size_t j = 0; j < N; j++) {
-        result[j] = -a1[j];
-    }
-    return result;
+constexpr auto operator-(std::array<T, N> const &a1) -> std::array<T, N> {
+  std::array<T, N> result{};
+  for (size_t j = 0; j < N; j++) {
+    result[j] = -a1[j];
+  }
+  return result;
 }
 
 // -m
 template <typename T, std::size_t N, std::size_t M>
-constexpr auto operator-(std::array<std::array<T, M>, N> const& m
-) -> std::array<std::array<T, M>, N>
-{
-    std::array<std::array<T, M>, N> result{};
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < M; j++) {
-            result[i][j] = -m[i][j];
-        }
+constexpr auto operator-(std::array<std::array<T, M>, N> const &m)
+    -> std::array<std::array<T, M>, N> {
+  std::array<std::array<T, M>, N> result{};
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < M; j++) {
+      result[i][j] = -m[i][j];
     }
-    return result;
+  }
+  return result;
 }
 
 // v * c
 template <typename T, std::size_t N>
-constexpr auto
-operator*(std::array<T, N> const& a, T const v) -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    for (size_t j = 0; j < N; j++) {
-        result[j] = v * a[j];
-    }
-    return result;
+constexpr auto operator*(std::array<T, N> const &a,
+                         T const v) -> std::array<T, N> {
+  std::array<T, N> result{};
+  for (size_t j = 0; j < N; j++) {
+    result[j] = v * a[j];
+  }
+  return result;
 }
 
 // m * c
 template <typename T, std::size_t N, std::size_t M>
-constexpr auto operator*(std::array<std::array<T, M>, N> const& m, T const& v)
-    -> std::array<std::array<T, M>, N>
-{
-    std::array<std::array<T, M>, N> result{};
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < M; j++) {
-            result[i][j] = v * m[i][j];
-        }
+constexpr auto operator*(std::array<std::array<T, M>, N> const &m,
+                         T const &v) -> std::array<std::array<T, M>, N> {
+  std::array<std::array<T, M>, N> result{};
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < M; j++) {
+      result[i][j] = v * m[i][j];
     }
-    return result;
+  }
+  return result;
 }
 
 // c * v
 template <typename T, std::size_t N>
-constexpr auto
-operator*(T const v, std::array<T, N> const& a) -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    for (size_t j = 0; j < N; j++) {
-        result[j] = v * a[j];
-    }
-    return result;
+constexpr auto operator*(T const v,
+                         std::array<T, N> const &a) -> std::array<T, N> {
+  std::array<T, N> result{};
+  for (size_t j = 0; j < N; j++) {
+    result[j] = v * a[j];
+  }
+  return result;
 }
 
 // c * m
 template <typename T, std::size_t N, std::size_t M>
-constexpr auto operator*(T const& v, std::array<std::array<T, M>, N> const& m)
-    -> std::array<std::array<T, M>, N>
-{
-    std::array<std::array<T, M>, N> result{};
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < M; j++) {
-            result[i][j] = v * m[i][j];
-        }
+constexpr auto operator*(T const &v, std::array<std::array<T, M>, N> const &m)
+    -> std::array<std::array<T, M>, N> {
+  std::array<std::array<T, M>, N> result{};
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < M; j++) {
+      result[i][j] = v * m[i][j];
     }
-    return result;
+  }
+  return result;
 }
 
 // m * v
 template <typename T, std::size_t N>
-constexpr auto operator*(
-    std::array<std::array<T, N>, N> const& mat,
-    std::array<T, N> const& vec
-) -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    for (size_t i = 0; i < N; i++) {
-        T sum = static_cast<T>(0);
-        for (size_t j = 0; j < N; j++) {
-            sum += mat[i][j] * vec[j];
-        }
-        result[i] = sum;
+constexpr auto operator*(std::array<std::array<T, N>, N> const &mat,
+                         std::array<T, N> const &vec) -> std::array<T, N> {
+  std::array<T, N> result{};
+  for (size_t i = 0; i < N; i++) {
+    T sum = static_cast<T>(0);
+    for (size_t j = 0; j < N; j++) {
+      sum += mat[i][j] * vec[j];
     }
-    return result;
+    result[i] = sum;
+  }
+  return result;
 }
 
 // m1 * m2
 template <typename T, std::size_t N, std::size_t M>
-constexpr auto operator*(
-    std::array<std::array<T, N>, M> const& m1,
-    std::array<std::array<T, M>, N> const& m2
-) -> std::array<std::array<T, M>, M>
-{
-    std::array<std::array<T, M>, M> result{};
-    for (size_t i = 0; i < M; i++) {
-        for (size_t j = 0; j < M; j++) {
-            for (size_t k = 0; k < N; k++) {
-                result[i][j] += m1[i][k] * m2[k][j];
-            }
-        }
+constexpr auto operator*(std::array<std::array<T, N>, M> const &m1,
+                         std::array<std::array<T, M>, N> const &m2)
+    -> std::array<std::array<T, M>, M> {
+  std::array<std::array<T, M>, M> result{};
+  for (size_t i = 0; i < M; i++) {
+    for (size_t j = 0; j < M; j++) {
+      for (size_t k = 0; k < N; k++) {
+        result[i][j] += m1[i][k] * m2[k][j];
+      }
     }
-    return result;
+  }
+  return result;
 }
 
 // v1 dot v2
 template <typename T, std::size_t N>
-constexpr auto dot(std::array<T, N> const& a1, std::array<T, N> const& a2) -> T
-{
-    return std::inner_product(
-        a1.begin(),
-        a1.end(),
-        a2.begin(),
-        static_cast<T>(0)
-    );
+constexpr auto dot(std::array<T, N> const &a1,
+                   std::array<T, N> const &a2) -> T {
+  return std::inner_product(a1.begin(), a1.end(), a2.begin(),
+                            static_cast<T>(0));
 }
 
 // transpose m
 template <typename T, std::size_t N>
-constexpr auto transpose(std::array<std::array<T, N>, N> const& a
-) -> std::array<std::array<T, N>, N>
-{
-    std::array<std::array<T, N>, N> result;
-    for (std::size_t i = 0; i < N; i++) {
-        for (std::size_t j = 0; j < N; j++) {
-            result[i][j] = a[j][i];
-        }
+constexpr auto transpose(std::array<std::array<T, N>, N> const &a)
+    -> std::array<std::array<T, N>, N> {
+  std::array<std::array<T, N>, N> result;
+  for (std::size_t i = 0; i < N; i++) {
+    for (std::size_t j = 0; j < N; j++) {
+      result[i][j] = a[j][i];
     }
-    return result;
+  }
+  return result;
 }
 
 // v1 cross v2
 template <typename T>
-constexpr auto cross(std::array<T, 3> const& v1, std::array<T, 3> const& v2)
-    -> std::array<T, 3>
-{
-    std::array<T, 3> result{};
-    result[0] = v1[1] * v2[2] - v1[2] * v2[1];
-    result[1] = v1[2] * v2[0] - v1[0] * v2[2];
-    result[2] = v1[0] * v2[1] - v1[1] * v2[0];
-    return result;
+constexpr auto cross(std::array<T, 3> const &v1,
+                     std::array<T, 3> const &v2) -> std::array<T, 3> {
+  std::array<T, 3> result{};
+  result[0] = v1[1] * v2[2] - v1[2] * v2[1];
+  result[1] = v1[2] * v2[0] - v1[0] * v2[2];
+  result[2] = v1[0] * v2[1] - v1[1] * v2[0];
+  return result;
 }
 
 // magnitude v
 template <typename T, std::size_t N>
-constexpr auto magnitude(std::array<T, N> const& v) -> T
-{
-    T acc{};
-    for (size_t i = 0; i < N; i++) {
-        acc += v[i] * v[i];
-    }
-    return std::sqrt(acc);
+constexpr auto magnitude(std::array<T, N> const &v) -> T {
+  T acc{};
+  for (size_t i = 0; i < N; i++) {
+    acc += v[i] * v[i];
+  }
+  return std::sqrt(acc);
 }
 
 // normalize v
 template <typename T, std::size_t N>
-constexpr auto normalize(std::array<T, N> const& v) -> std::array<T, N>
-{
-    std::array<T, N> result{};
-    double const length = magnitude(v);
-    for (size_t i = 0; i < N; i++) {
-        result[i] = v / length;
-    }
-    return result;
+constexpr auto normalize(std::array<T, N> const &v) -> std::array<T, N> {
+  std::array<T, N> result{};
+  double const length = magnitude(v);
+  for (size_t i = 0; i < N; i++) {
+    result[i] = v / length;
+  }
+  return result;
 }
+
+// Tranformations needed for graphics
+namespace aa {
+
+template <typename T>
+auto constexpr scale(T x, T y, T z) -> std::array<std::array<T, 4>, 4> {
+  return std::array<std::array<T, 4>, 4>{
+      {{x, static_cast<T>(0), static_cast<T>(0), static_cast<T>(0)},
+       {static_cast<T>(0), y, static_cast<T>(0), static_cast<T>(0)},
+       {static_cast<T>(0), static_cast<T>(0), z, static_cast<T>(0)},
+       {static_cast<T>(0), static_cast<T>(0), static_cast<T>(0),
+        static_cast<T>(1)}}};
+}
+
+template <typename T>
+auto constexpr translate(std::array<T, 3> translation_vector)
+    -> std::array<std::array<T, 4>, 4> {
+  return std::array<std::array<T, 4>, 4>{
+      {{static_cast<T>(1), static_cast<T>(0), static_cast<T>(0),
+        translation_vector[0]},
+       {static_cast<T>(0), static_cast<T>(1), static_cast<T>(0),
+        translation_vector[1]},
+       {static_cast<T>(0), static_cast<T>(0), static_cast<T>(1),
+        translation_vector[2]},
+       {static_cast<T>(0), static_cast<T>(0), static_cast<T>(0),
+        static_cast<T>(1)}}};
+}
+
+} // namespace aa
 
 #endif
